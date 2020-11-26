214644170bbc4eb6ae202dae21deaaae6ba5b31d
Ratchakorn Suraphatcharadecha <ratchakorn.s@facgure.com>
2020-11-26 22:44:51 +0700

    Add asset file

---
+++ lib/src/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'screen/home.dart';
+
+class OpenTalk extends StatelessWidget {
+  const OpenTalk({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Home(),
+    );
+  }
+}
